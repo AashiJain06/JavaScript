28c5effcf49868702ac81c68d81923667168eedf
Aashi Jain <aashijain064@gmail.com>
2025-06-24 22:52:09 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,11 @@
+//let num = document.getElementsById.value("number");
+print();
+function print()
+{
+  let num =2;
+  for(let i=0;i<10;i++)
+  {
+    document.write('2 x ${i} = *${num*i}');
+    document.write("<br>");
+  }
+}
