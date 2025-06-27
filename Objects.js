7b7abf0e735e8fa1cd880dcb81b4f0388378caac
Aashi Jain <aashijain064@gmail.com>
2025-06-27 22:31:52 +0530

    Add files via upload

---
+++ Objects.js
@@ -0,0 +1,8 @@
+let person = {
+   firstname :"Aashi",
+   lastname : "Jain"
+};
+console.log(person.firstname);
+//Array Accessing method
+console.log(person['firstname']);
+console.log(person.age);
