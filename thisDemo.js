5cfcd1950bde954b7b0b64eb98e04548a1d6f213
Aashi Jain <aashijain064@gmail.com>
2025-06-28 22:46:52 +0530

    Add files via upload

---
+++ thisDemo.js
@@ -0,0 +1,13 @@
+let person = {
+  firstname : "Aashi",
+  lastname : "Jain",
+  sayHello() 
+  {
+    console.log("Hello i am "+this.firstname+" and i have "+car.brand+"car");
+  }
+};
+let car = {
+  brand :"Tata",
+  model : "Safari"
+};
+person.sayHello();
